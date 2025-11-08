733d29cda424ad510d3a994ea37aa31f4a80fc7a
Shubh <shubhkakadia@gmail.com>
2025-11-08 15:44:52 +1030

    pipeline nginx 4

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
       {
         protocol: 'http',
         hostname: 'localhost',
-        port: '3000',
+        port: process.env.NEXT_APP_PORT,
         pathname: '/api/media/**',
       },
       {
